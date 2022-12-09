977ead9e6fb73682b3f46bc98635338779fec1b5
Alina Kvasova <al.kvasovaa@yandex.ru>
2022-12-10 00:11:44 +0700

    создала кастомный хук для inputa

--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -1,21 +1,34 @@
 import React, {useState} from "react";
 import PropTypes from 'prop-types'
 
+function useInputValue(defaultValue = '') {//Кастомный хук для input
+    const [value, setValue] = useState(defaultValue) //логика по опред value
+
+    return {//в качестве значения вернем объект. Объект к-ый возвращаем из данного хука, содержит ключи для inputa
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value) //значение onChange, функция к-ая принимает event, она будет обращаться к методу setValue и задавать знач event.target.value 
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+}
+
 function AddTodo( {onCreate} ) {
-    const [value, setValue] = useState('')
+    const input = useInputValue('')//воспользуемся своим хуком 
 
     function submitHandler(event) {
-        event.preventDefault()/*отменяет дефолтное значение, чтобы страница не перезагружалась */
+        event.preventDefault()//отменяет дефолтное значение, чтобы страница не перезагружалась 
 
-        if (value.trim()) {/*валидация, трим удаляет лишние пробелы*/
-            onCreate(value)
-            setValue('')/*чтобы очищалось поле после добавления эл-та, вызовем метод setValue со знач пустой строки т.е изм данный стейт */
+        if (input.value().trim()) {//валидация, трим удаляет лишние пробелы. Чтобы получить знач обр к функ value()
+            onCreate(input.value())//после создания хука, нету параметра value, но есть input со знач value
+            input.clear()//чтобы очищалось поле после добавления эл-та, обр к функ clear()
         }
     }
 
-    return (
+    return (//с помощью оператора спред ... развернули объект inputa. т.е он поместит value и onChange
         <form style={{marginBottom: '1rem' }} onSubmit={submitHandler}>
-            <input value={value} onChange={event => setValue(event.target.value)} />
+            <input {...input.bind} /> 
             <button type='submit'>Add todo</button>
         </form>
     )
