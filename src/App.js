bdaa6056ea9b3964a64aaa17a2e7dd941b49bd58
Alina Kvasova <al.kvasovaa@yandex.ru>
2022-12-09 23:26:05 +0700

    форма для доб нового элемента

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
+import AddTodo from './Todo/AddTodo'
 
 
 function App() {
@@ -21,19 +22,39 @@ function toggleTodo(id) {
   )
 }
 
+/*Удаляем строки*/
 function removeTodo(id) {
   setTodos(todos.filter(todo => todo.id !== id))
+}
+
+function addTodo(title) {
+  setTodos/*чтобы изменить стейт*/(
+    todos.concat([/*метод concat, чтобы добавить нужный эл-нет*/
+      { 
+        title,
+        id: Date.now(),
+        completed:false
+      }
+    ])
+  )
 }
 
   return (
     <Context.Provider value={{removeTodo: removeTodo}}>
       <div className='wrapper'>
         <h1>React tutorial</h1>
+        <AddTodo onCreate={addTodo}/>
+        
+        {todos.length ? (/*Условие показываем нет дел*/
+          <TodoList todos={todos} onToggle={toggleTodo} />
+        ) : (
+          <p>No todos!</p>
+        )}
 
-        <TodoList todos={todos} onToggle={toggleTodo} />
+        
       </div>
     </Context.Provider>
-  );
+  )
 }
 
 export default App;
