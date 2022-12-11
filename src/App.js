8010372df4d0eefa66ce282d22aac062a7b7aa12
Alina Kvasova <al.kvasovaa@yandex.ru>
2022-12-11 14:53:05 +0700

    добавила Loader

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, {useEffect} from 'react'
 import TodoList from './Todo/TodoList'
 import Context from './context'
 import AddTodo from './Todo/AddTodo'
+import Loader from './Loader'
 
 
 function App() {
   const [todos, setTodos] = React.useState([])
+  const [loading, setLoading] = React.useState(true) //показывает лоадер, если идет процесс загрузки. заведем стейт, чтобы следить за лоадингом
 
   useEffect(() => {//работа с сервером
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5') //обращаемся к фетч, возвр список todos. ?_limit=5 квери параметр, чтобы загрузилось 5 элементов
       .then(response => response.json())
       .then(todos => {//получаем массив тудус 
-        setTodos(todos)//чтобы изменить стейт, обращаемся к функции setTodos и передаем массив todos 
+        setTimeout(() => {//чтобы сервер отвечал с задержкой в 2 сек
+          setTodos(todos)//чтобы изменить стейт, обращаемся к функции setTodos и передаем массив todos 
+          setLoading(false)//убираем лоадер после загрузки
+        }, 2000)
       })
   }, [])
 
@@ -50,9 +55,12 @@ function App() {
         <h1>React tutorial</h1>
         <AddTodo onCreate={addTodo}/>
         
+        
+      {loading && <Loader/*если лоадинг находится в тру, показываем компонент Loading*/ />}
+        
         {todos.length ? (/*Условие показываем нет дел*/
           <TodoList todos={todos} onToggle={toggleTodo} />
-        ) : (
+        ) : loading ? null : (//если лоадинг, тогда ничего не выводим, иначе выводим нет дел
           <p>No todos!</p>
         )}
 
